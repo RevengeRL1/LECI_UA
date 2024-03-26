9c3b6bc1ac2f7f4d47ccc6aeec69b87e9ec5dfec
Tiago <tandrecosta@ua.pt>
2024-03-26 16:32:16 +0000

    POO - Guiao 4 completo

--- 1_Ano/2_Semestre/POO/src/aula04/CarDemo.java
+++ 1_Ano/2_Semestre/POO/src/aula04/CarDemo.java
@@ -1,3 +1,5 @@
+package aula04;
+
 import java.util.Scanner;
 
 class Car {
@@ -14,6 +16,7 @@ class Car {
     }
 
     public void drive(int distance) {
+        kms += distance;
         //TODO: acumular distância percorrida
     }
 
@@ -24,26 +27,54 @@ public class CarDemo {
     static Scanner sc = new Scanner(System.in);
 
     static int registerCars(Car[] cars) {
-        // TODO: pede dados dos carros ao utilizador e acrescenta ao vetor
-        // registo de carros termina quando o utilizador inserir uma linha vazia 
-        // devolve número de carros registados
-        System.out.print("Insira dados do carro (marca modelo ano quilómetros): ");
+        String answer, make, model;
+        int year, kms;
+        int carCount = 0;
+        while(true){
+            System.out.print("Insert car manufacturer, model, year and kilometers separated by a comma (blank to stop): ");
+            answer = sc.nextLine();
+            if(answer == ""){ break; }
+            String[] values = answer.split(",");
+            make = values[0];
+            model = values[1];
+            year = Integer.parseInt(values[2]);
+            kms = Integer.parseInt(values[3]);
+            cars[carCount] = new Car(make, model, year, kms);
+            carCount++;
+        }
+        return carCount;
    }
 
     static void registerTrips(Car[] cars, int numCars) {
         // TODO: pede dados das viagens ao utilizador e atualiza informação do carro
-        // registo de viagens termina quando o utilizador inserir uma linha vazia 
-        System.out.print("Registe uma viagem no formato \"carro:distância\": ");
+        // registo de viagens termina quando o utilizador inserir uma linha vazia
+        while(true){ 
+            System.out.print("Register a trip using the car:distance format (blank to stop):  ");
+            String answer = sc.nextLine();
+            if(answer == ""){ break; }
+            String[] values = answer.split(":");
+            int car = Integer.parseInt(values[0]);
+            int distance = Integer.parseInt(values[1]);
+            if(car > numCars || car < 0){ continue; }
+            cars[car].drive(distance);
+        }
+
+
     }
 
 
     static void listCars(Car[] cars) {
-        System.out.println("\nCarros registados: ");
         // TODO: lista todos os carros registados
         // Exemplo de resultado
         // Carros registados: 
         // Toyota Camry, 2010, kms: 234346
         // Renault Megane Sport Tourer, 2015, kms: 32536
+        System.out.println("Registered cars: ");
+        for (Car car : cars){
+            if (car != null){
+                System.out.println(car);
+            }
+        }
         
         System.out.println("\n");
     }
